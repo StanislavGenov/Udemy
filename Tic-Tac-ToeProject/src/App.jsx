58e7aa522ef848b7ab7ac57cce6dafce5d97bbcc
Stanislav Genov <69149102+StanislavGenov@users.noreply.github.com>
2024-05-29 18:15:16 +0300

    almost finished project

--- Tic-Tac-ToeProject/src/App.jsx
+++ Tic-Tac-ToeProject/src/App.jsx
@@ -1,10 +1,83 @@
+import { useState } from 'react';
 
-function App() {
-  
+import Player from './components/Player.jsx'
+import GameBoard from './components/Gameboard.jsx';
+import Log from './components/Log.jsx';
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+import GameOver from './components/GameOver.jsx';
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X'){
+   currentPlayer= 'O'; 
+  }
+  return currentPlayer;
 }
 
-export default App
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  //const [hasWinner, setHasWinner] = useState(false);
+  //const [activePlayer, setActivePlayer] = useState('X');
+    
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
+
+  for(const turn of gameTurns){
+      const { square, player}= turn;
+      const { row, col} = square;
+     
+      
+      gameBoard[row][col] = player;
+  }
+   let winner = null;
+   
+   for (const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]; 
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+         winner = firstSquareSymbol;
+    }
+   }
+
+    const hasDraw = gameTurns.length === 9 && !winner;
+
+  function HandleSelectSquare(rowIndex, colIndex) {
+    //setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurn => {
+       const currentPlayer = deriveActivePlayer(prevTurn);
+
+      const updatedTurn = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer}, 
+        ...prevTurn];
+        return updatedTurn;
+    });
+  }
+  function handleRematch(){
+    setGameTurns([]);
+  }
+  return (
+  <main>
+    <div id="game-container">
+      <ol id="players" className='highlight-player'>
+      <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+      <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+      </ol>
+      {(winner || hasDraw) && (<GameOver winner={winner} onRestart={handleRematch}/>)}
+      <GameBoard onSelectSquare={HandleSelectSquare} board={gameBoard}/>
+    </div>
+    <Log turns={gameTurns}/>
+    </main>
+    );
+}
+
+export default App;
